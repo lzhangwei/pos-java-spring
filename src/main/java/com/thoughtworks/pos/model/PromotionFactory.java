ee01e1433209a8ef77d534d489f406a720e94f26
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 23:25:02 +0800

    add getPromotionByType function

--- src/main/java/com/thoughtworks/pos/model/PromotionFactory.java
+++ src/main/java/com/thoughtworks/pos/model/PromotionFactory.java
@@ -1,4 +1,24 @@
 package com.thoughtworks.pos.model;
 
 public class PromotionFactory {
+    public static final int PROMOTION_DISCOUNT = 1;
+    public static final int PROMOTION_SECOND_HALF = 2;
+    public static final int PROMOTION_TWO_FOR_ONE = 3;
+
+    public static Promotion getPromotionByType(int type){
+        Promotion promotion = null;
+        switch(type){
+            case PROMOTION_DISCOUNT:
+                promotion = new PromotionDiscount();
+                break;
+            case PROMOTION_SECOND_HALF:
+                promotion = new PromotionSecondHalf();
+                break;
+            case PROMOTION_TWO_FOR_ONE:
+                promotion = new PromotionTwoForOne();
+                break;
+
+        }
+        return promotion;
+    }
 }
