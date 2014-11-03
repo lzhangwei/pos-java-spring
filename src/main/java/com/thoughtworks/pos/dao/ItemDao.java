ad27bc4f40af8f83dca73d31d42cf7161562fdc7
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 22:38:13 +0800

    add function in itemDao

--- src/main/java/com/thoughtworks/pos/dao/ItemDao.java
+++ src/main/java/com/thoughtworks/pos/dao/ItemDao.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.pos.dao;
 
+import com.thoughtworks.pos.model.Item;
+
+import java.util.List;
+
 public interface ItemDao {
+
+    List<Item> getItems();
+
+    Item getItemByBarcode(String barcode);
+
 }
--- src/main/java/com/thoughtworks/pos/dao/impl/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/pos/dao/impl/ItemDaoImpl.java
@@ -1,6 +1,18 @@
 package com.thoughtworks.pos.dao.impl;
 
 import com.thoughtworks.pos.dao.ItemDao;
+import com.thoughtworks.pos.model.Item;
+
+import java.util.List;
 
 public class ItemDaoImpl implements ItemDao{
+    @Override
+    public List<Item> getItems() {
+        return null;
+    }
+
+    @Override
+    public Item getItemByBarcode(String barcode) {
+        return null;
+    }
 }
