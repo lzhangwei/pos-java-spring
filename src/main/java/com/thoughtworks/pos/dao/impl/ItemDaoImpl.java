019db0d23bc773357fefdcefcad16493b73fcc5b
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 22:40:32 +0800

    add dataSource and nameParameterJdbcTemplate

--- src/main/java/com/thoughtworks/pos/dao/impl/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/pos/dao/impl/ItemDaoImpl.java
@@ -2,10 +2,21 @@ package com.thoughtworks.pos.dao.impl;
 
 import com.thoughtworks.pos.dao.ItemDao;
 import com.thoughtworks.pos.model.Item;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 public class ItemDaoImpl implements ItemDao{
+
+    private DataSource dataSource;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public ItemDaoImpl(DataSource dataSource, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.dataSource = dataSource;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public List<Item> getItems() {
         return null;
