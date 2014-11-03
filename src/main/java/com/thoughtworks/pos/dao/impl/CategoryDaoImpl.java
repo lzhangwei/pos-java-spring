ba265dbdff27111b77b4178871f1ae3b4a981a1b
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 20:26:23 +0800

    add getCategoryById

--- src/main/java/com/thoughtworks/pos/dao/impl/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/pos/dao/impl/CategoryDaoImpl.java
@@ -1,6 +1,11 @@
 package com.thoughtworks.pos.dao.impl;
 
 import com.thoughtworks.pos.dao.CategoryDao;
+import com.thoughtworks.pos.model.Category;
 
 public class CategoryDaoImpl implements CategoryDao{
+    @Override
+    public Category getCategoryById(int id) {
+        return null;
+    }
 }
