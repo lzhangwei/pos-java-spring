ae20c1ebd753c7d42a7a9d3982a19bbe896f68f4
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 22:20:08 +0800

    update CategoryDaoImpl

--- src/main/java/com/thoughtworks/pos/dao/impl/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/pos/dao/impl/CategoryDaoImpl.java
@@ -2,60 +2,54 @@ package com.thoughtworks.pos.dao.impl;
 
 import com.thoughtworks.pos.dao.CategoryDao;
 import com.thoughtworks.pos.model.Category;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CategoryDaoImpl implements CategoryDao{
+public class CategoryDaoImpl implements CategoryDao, InitializingBean{
 
     private DataSource dataSource;
-    
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public CategoryDaoImpl(DataSource dataSource, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.dataSource = dataSource;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public Category getCategoryById(int id) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        String sql = "SELECT * FROM categories WHERE id=?";
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, id);
-            resultSet = preparedStatement.executeQuery();
-            Category category = null;
-            if(resultSet.next()) {
-                category = new Category();
-                category.setId(resultSet.getInt("id"));
-                category.setName(resultSet.getString("name"));
-            }
-            return category;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if(resultSet != null){
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+        String sql = "SELECT * FROM categories WHERE id = :categoryId";
+
+        Map<String, Object> namedParameters = new HashMap<String, Object>();
+        namedParameters.put("categoryId", id);
+
+        return namedParameterJdbcTemplate.query(sql, namedParameters, new CategoryMapper()).get(0);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if(dataSource == null) {
+            throw new BeanCreationException("Must set DataSource on CategoryDaoImpl");
         }
-        return null;
+        if(namedParameterJdbcTemplate == null) {
+            throw new BeanCreationException("Null NamedParameterJdbcTemplate on CategoryDaoImpl");
+        }
+    }
+
+    private static final class CategoryMapper implements RowMapper<Category> {
+        @Override
+        public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Category contact = new Category();
+            contact.setId(rs.getInt("id"));
+            contact.setName(rs.getString("name"));
+            return contact;
+        }
     }
 }
