850e7ec75fc92d2443097452eb0f4ca9e16e67e3
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 23:35:39 +0800

    getItems and getItemByBarcode

--- src/main/java/com/thoughtworks/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/pos/service/ItemService.java
@@ -1,4 +1,11 @@
 package com.thoughtworks.pos.service;
 
+import com.thoughtworks.pos.model.Item;
+
+import java.util.List;
+
 public interface ItemService {
+    List<Item> getItems();
+
+    Item getItemByBarcode(String barcode);
 }
--- src/main/java/com/thoughtworks/pos/service/ItemServiceImpl.java
+++ src/main/java/com/thoughtworks/pos/service/ItemServiceImpl.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.pos.service;
 
+import com.thoughtworks.pos.model.Item;
+
+import java.util.List;
+
 public class ItemServiceImpl implements ItemService{
+    @Override
+    public List<Item> getItems() {
+        return null;
+    }
+
+    @Override
+    public Item getItemByBarcode(String barcode) {
+        return null;
+    }
 }
