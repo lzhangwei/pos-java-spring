fb7f8c34b1b85008f0fd2094169edc16ff2b3ad1
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 16:57:44 +0800

    first commit

---
+++ src/main/java/com/thoughtworks/pos/HelloApp.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.pos;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+
+    }
+}
