ca645540280ada3fc5d26750cfd0831298da8621
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 21:05:31 +0800

    update category dao

--- src/main/java/com/thoughtworks/pos/dao/impl/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/pos/dao/impl/CategoryDaoImpl.java
@@ -3,9 +3,59 @@ package com.thoughtworks.pos.dao.impl;
 import com.thoughtworks.pos.dao.CategoryDao;
 import com.thoughtworks.pos.model.Category;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CategoryDaoImpl implements CategoryDao{
+
+    private DataSource dataSource;
+    
     @Override
     public Category getCategoryById(int id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        String sql = "SELECT * FROM categories WHERE id=?";
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            Category category = null;
+            if(resultSet.next()) {
+                category = new Category();
+                category.setId(resultSet.getInt("id"));
+                category.setName(resultSet.getString("name"));
+            }
+            return category;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(resultSet != null){
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 }
--- src/main/java/com/thoughtworks/pos/model/Category.java
+++ src/main/java/com/thoughtworks/pos/model/Category.java
@@ -3,4 +3,12 @@ package com.thoughtworks.pos.model;
 public class Category {
     private int id;
     private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
