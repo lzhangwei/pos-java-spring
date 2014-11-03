306b8dc820ffcb3934f2e6bbba7b2773d1fc0704
lzhangwei <zhangwei_999@foxmail.com>
2014-11-03 17:15:23 +0800

    add Scanner class

---
+++ src/main/java/com/thoughtworks/pos/util/Scanner.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.pos.util;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Scanner {
+    public List<String> readFile(String pathstr) {
+        Path path = Paths.get(pathstr);
+        List<String> result = null;
+        try {
+            result = Files.readAllLines(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
