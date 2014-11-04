f2b8a4f54ac5b111834d74c34f44ce207d34d837
lzhangwei <zhangwei_999@foxmail.com>
2014-11-04 08:49:32 +0800

    update parseBarcode

--- src/main/java/com/thoughtworks/pos/Pos.java
+++ src/main/java/com/thoughtworks/pos/Pos.java
@@ -1,4 +1,63 @@
 package com.thoughtworks.pos;
 
+import com.thoughtworks.pos.model.CartItem;
+import com.thoughtworks.pos.model.CategoryList;
+import com.thoughtworks.pos.model.Item;
+import com.thoughtworks.pos.service.ItemService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pos {
+    private List<Item> items;
+    private List<CartItem> cartItems;
+    private double sumPrice;
+    private double promotionPrice;
+    private ItemService itemService;
+
+    public void setItemService(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    public void parseBarcode(List<String> cartItemBarcodes) {
+        for (String barcode : cartItemBarcodes) {
+            String[] splitBarcode = barcode.split("-");
+            Item item = itemService.getItemByBarcode(splitBarcode[0]);
+            items.add(item);
+            double num = splitBarcode.length == 1 ? 1 : Double.parseDouble(splitBarcode[1]);
+            cartItems.add(new CartItem(item, num));
+        }
+
+        cartItems = mergeCartItems(cartItems);
+    }
+
+    private List<CartItem> mergeCartItems(List<CartItem> cartItems) {
+        for (int i = 0; i < cartItems.size() - 1; i++) {
+            for (int j = i + 1; j < cartItems.size(); j++) {
+                if (cartItems.get(i).getBarcode().equals(cartItems.get(j).getBarcode())) {
+                    double newNum = cartItems.get(i).getNum() + cartItems.get(j).getNum();
+                    cartItems.get(i).setNum(newNum);
+                    cartItems.remove(j);
+                    i--;
+                    break;
+                }
+            }
+        }
+        return cartItems;
+    }
+
+    public void caculatePrice() {
+    }
+
+    public double getSumPrice() {
+        return sumPrice;
+    }
+
+    public double getPromotionPrice() {
+        return promotionPrice;
+    }
+
+    public List<CategoryList> createCategoryLists() {
+        return null;
+    }
 }
