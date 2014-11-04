0c198cc59161c11faae99aba1aae1b3bfa6cb20d
lzhangwei <zhangwei_999@foxmail.com>
2014-11-04 08:37:21 +0800

    update getItems function

--- src/main/java/com/thoughtworks/pos/service/ItemServiceImpl.java
+++ src/main/java/com/thoughtworks/pos/service/ItemServiceImpl.java
@@ -3,8 +3,11 @@ package com.thoughtworks.pos.service;
 import com.thoughtworks.pos.dao.CategoryDao;
 import com.thoughtworks.pos.dao.ItemDao;
 import com.thoughtworks.pos.dao.PromotionDao;
+import com.thoughtworks.pos.model.Category;
 import com.thoughtworks.pos.model.Item;
+import com.thoughtworks.pos.model.Promotion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemServiceImpl implements ItemService{
@@ -21,7 +24,17 @@ public class ItemServiceImpl implements ItemService{
 
     @Override
     public List<Item> getItems() {
-        return null;
+        List<Item> items = itemDao.getItems();
+        for(Item item : items) {
+            Category category = categoryDao.getCategoryById(item.getCategoryId());
+            item.setCategory(category);
+
+            List<Promotion> promotionList = promotionDao.getPromotionsByItemId(item.getId());
+            item.setPromotionList(promotionList);
+
+            item.setDiscount(promotionDao.getPromotionDiscount(item.getId()));
+        }
+        return items;
     }
 
     @Override
