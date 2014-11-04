323312971516f65cf395a41411c102c31c3bc3a1
lzhangwei <zhangwei_999@foxmail.com>
2014-11-04 09:29:02 +0800

    update caculate function

--- src/main/java/com/thoughtworks/pos/model/PromotionSecondHalf.java
+++ src/main/java/com/thoughtworks/pos/model/PromotionSecondHalf.java
@@ -3,6 +3,6 @@ package com.thoughtworks.pos.model;
 public class PromotionSecondHalf extends Promotion{
     @Override
     public double caculatePromotionPrice(CartItem cartItem) {
-        return 0;
+        return (int)cartItem.getNum() / 2 * (cartItem.getPrice() / 2.0);
     }
 }
