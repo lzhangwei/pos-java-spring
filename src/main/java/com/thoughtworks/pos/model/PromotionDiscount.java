8faaec5cd8aae91ab27ac322715388ba5fe4d14a
lzhangwei <zhangwei_999@foxmail.com>
2014-11-04 09:27:26 +0800

    update caculate

--- src/main/java/com/thoughtworks/pos/model/PromotionDiscount.java
+++ src/main/java/com/thoughtworks/pos/model/PromotionDiscount.java
@@ -3,6 +3,6 @@ package com.thoughtworks.pos.model;
 public class PromotionDiscount extends Promotion{
     @Override
     public double caculatePromotionPrice(CartItem cartItem) {
-        return 0;
+        return cartItem.getNum() * cartItem.getPrice() * ((100 - cartItem.getDiscount())) / 100.0;
     }
 }
