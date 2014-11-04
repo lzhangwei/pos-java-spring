8290c5a29efed7531cb00992b4fc96a21c612965
lzhangwei <zhangwei_999@foxmail.com>
2014-11-04 08:30:00 +0800

    add dao

--- src/main/java/com/thoughtworks/pos/service/ItemServiceImpl.java
+++ src/main/java/com/thoughtworks/pos/service/ItemServiceImpl.java
@@ -1,10 +1,24 @@
 package com.thoughtworks.pos.service;
 
+import com.thoughtworks.pos.dao.CategoryDao;
+import com.thoughtworks.pos.dao.ItemDao;
+import com.thoughtworks.pos.dao.PromotionDao;
 import com.thoughtworks.pos.model.Item;
 
 import java.util.List;
 
 public class ItemServiceImpl implements ItemService{
+
+    private ItemDao itemDao;
+    private CategoryDao categoryDao;
+    private PromotionDao promotionDao;
+
+    public ItemServiceImpl(ItemDao itemDao, CategoryDao categoryDao, PromotionDao promotionDao) {
+        this.itemDao = itemDao;
+        this.categoryDao = categoryDao;
+        this.promotionDao = promotionDao;
+    }
+
     @Override
     public List<Item> getItems() {
         return null;
