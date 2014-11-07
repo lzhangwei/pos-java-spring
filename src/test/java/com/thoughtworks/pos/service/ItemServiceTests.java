5ca874f58e56c9c5d02f27a1c9d4343a9f3031f8
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 11:47:37 +0800

    add test function

--- src/test/java/com/thoughtworks/pos/service/ItemServiceTests.java
+++ src/test/java/com/thoughtworks/pos/service/ItemServiceTests.java
@@ -1,4 +1,25 @@
 package com.thoughtworks.pos.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-config.xml")
 public class ItemServiceTests {
+
+    @Autowired
+    private ItemService itemService;
+
+    @Test
+    public void get_all_items_test() {
+
+    }
+
+    @Test
+    public void get_item_by_barcode_test() {
+
+    }
 }
