a839ae7951117ed07735e3e02b7cc250dea5a405
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 11:40:14 +0800

    update scanner test

--- src/test/java/com/thoughtworks/pos/util/ScannerTests.java
+++ src/test/java/com/thoughtworks/pos/util/ScannerTests.java
@@ -1,4 +1,32 @@
 package com.thoughtworks.pos.util;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ScannerTests {
+    @Test
+    public void scanner_test() {
+        List<String> barcodes = new ArrayList<String>();
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000003-2");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000002");
+        barcodes.add("ITEM000002");
+        barcodes.add("ITEM000002");
+        String path = "src/main/resources/cart.txt";
+        Scanner scanner = new Scanner();
+        List<String> result = scanner.readFile(path);
+        assertThat(result.size()).isEqualTo(12);
+        assertThat(result.get(1)).isEqualTo("ITEM000001");
+    }
 }
