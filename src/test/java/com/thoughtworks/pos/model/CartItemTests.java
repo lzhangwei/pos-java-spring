470ef2d21c52bce063e12fc4cc1f1a50bb475673
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 16:49:03 +0800

    add caculate cartItem promotion test

--- src/test/java/com/thoughtworks/pos/model/CartItemTests.java
+++ src/test/java/com/thoughtworks/pos/model/CartItemTests.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.pos.model;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CartItemTests {
+
+    @Test
+    public void caculate_promotion_test() {
+        List<Promotion> promotions = new ArrayList<>();
+        promotions.add(new PromotionDiscount());
+        promotions.add(new PromotionSecondHalf());
+        promotions.add(new PromotionTwoForOne());
+        Item item = new Item(1, "ITEM000001", "雪碧", "瓶", 3.0, new Category(1, "饮料"), 75, promotions);
+        CartItem cartItem = new CartItem(item, 5);
+
+        assertThat(cartItem.calculatePromotionPrice()).isEqualTo(3.75);
+    }
 }
