eaf11193b86626dd8a8b6f351f6e0738f9f256ab
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 16:35:25 +0800

    add promotion factory test

--- src/test/java/com/thoughtworks/pos/model/PromotionTests.java
+++ src/test/java/com/thoughtworks/pos/model/PromotionTests.java
@@ -1,4 +1,25 @@
 package com.thoughtworks.pos.model;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class PromotionTests {
+
+    @Test
+    public void should_return_PromotionDiscount_object(){
+        assertThat(PromotionFactory.getPromotionByType(1) instanceof PromotionDiscount).isTrue();
+    }
+
+    @Test
+    public void should_return_PromotionSecondHalf_object(){
+        assertThat(PromotionFactory.getPromotionByType(2) instanceof PromotionSecondHalf).isTrue();
+    }
+
+    @Test
+    public void should_return_PromotionTwoForOne_object(){
+        assertThat(PromotionFactory.getPromotionByType(3) instanceof PromotionTwoForOne).isTrue();
+    }
+
+    
 }
