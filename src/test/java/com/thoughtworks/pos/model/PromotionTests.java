8808edae22256c10a6242fe06a96ddba3298f65e
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 17:31:43 +0800

    update promotion caculate test

--- src/test/java/com/thoughtworks/pos/model/PromotionTests.java
+++ src/test/java/com/thoughtworks/pos/model/PromotionTests.java
@@ -25,16 +25,32 @@ public class PromotionTests {
     }
 
     @Test
-    public void promotion_caculate_test() {
+    public void discount_promotion_caculate_test() {
         List<Promotion> promotions = new ArrayList<>();
         promotions.add(new PromotionDiscount());
-        promotions.add(new PromotionSecondHalf());
-        promotions.add(new PromotionTwoForOne());
         Item item = new Item(1, "ITEM000001", "雪碧", "瓶", 3.0, new Category(1, "饮料"), 75, promotions);
         CartItem cartItem = new CartItem(item, 5);
 
         assertThat(promotions.get(0).caculatePromotionPrice(cartItem)).isEqualTo(3.75);
-        assertThat(promotions.get(1).caculatePromotionPrice(cartItem)).isEqualTo(3);
-        assertThat(promotions.get(2).caculatePromotionPrice(cartItem)).isEqualTo(3);
+    }
+
+    @Test
+    public void second_half_promotion_caculate_test() {
+        List<Promotion> promotions = new ArrayList<>();
+        promotions.add(new PromotionSecondHalf());
+        Item item = new Item(1, "ITEM000001", "雪碧", "瓶", 3.0, new Category(1, "饮料"), 75, promotions);
+        CartItem cartItem = new CartItem(item, 5);
+
+        assertThat(promotions.get(0).caculatePromotionPrice(cartItem)).isEqualTo(3);
+    }
+
+    @Test
+    public void two_for_one_promotion_caculate_test() {
+        List<Promotion> promotions = new ArrayList<>();
+        promotions.add(new PromotionTwoForOne());
+        Item item = new Item(1, "ITEM000001", "雪碧", "瓶", 3.0, new Category(1, "饮料"), 75, promotions);
+        CartItem cartItem = new CartItem(item, 5);
+
+        assertThat(promotions.get(0).caculatePromotionPrice(cartItem)).isEqualTo(3);
     }
 }
