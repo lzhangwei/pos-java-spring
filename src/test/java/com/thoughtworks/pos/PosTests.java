7d652a8d9192914085d7ddef86ef21dce6f123a1
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 17:05:07 +0800

    add parse barcode to cartItem test

--- src/test/java/com/thoughtworks/pos/PosTests.java
+++ src/test/java/com/thoughtworks/pos/PosTests.java
@@ -5,18 +5,40 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class PosTests {
 
     private Pos pos;
+    List<String> barcodes;
 
     @Before
     public void before() {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         pos = (Pos)context.getBean("pos");
+
+        barcodes = new ArrayList<>();
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000003-2");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000002");
+        barcodes.add("ITEM000002");
+        barcodes.add("ITEM000002");
     }
 
     @Test
-    public void test() {
-
+    public void parse_barcode_to_CartItem_test() {
+        pos.parseBarcode(barcodes);
+        assertThat(pos.getCartItems().size()).isEqualTo(4);
+        assertThat(pos.getCartItems().get(2).getItemName()).isEqualTo("电池");
     }
 }
