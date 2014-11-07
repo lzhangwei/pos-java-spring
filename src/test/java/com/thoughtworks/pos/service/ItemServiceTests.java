a07db4ce067764bf61ab55a65a4be1a2a5840ab4
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 16:24:18 +0800

    update service test

--- src/test/java/com/thoughtworks/pos/service/ItemServiceTests.java
+++ src/test/java/com/thoughtworks/pos/service/ItemServiceTests.java
@@ -1,25 +1,43 @@
 package com.thoughtworks.pos.service;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring-config.xml")
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ItemServiceTests {
 
-    @Autowired
+
     private ItemService itemService;
+
+    @Before
+    public void before() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        itemService = (ItemService)context.getBean("itemService");
+    }
+
+
 
     @Test
     public void get_all_items_test() {
+
+        assertThat(itemService.getItems().size()).isEqualTo(6);
+        assertThat(itemService.getItems().get(1).getName()).isEqualTo("雪碧");
+        assertThat(itemService.getItems().get(1).getCategoryId()).isEqualTo(1);
+        assertThat(itemService.getItems().get(1).getDiscount()).isEqualTo(75);
+        assertThat(itemService.getItems().get(1).getPromotionList().size()).isEqualTo(3);
 
     }
 
     @Test
     public void get_item_by_barcode_test() {
+
+        assertThat(itemService.getItemByBarcode("ITEM000001").getName()).isEqualTo("雪碧");
+        assertThat(itemService.getItemByBarcode("ITEM000001").getCategoryId()).isEqualTo(1);
+        assertThat(itemService.getItemByBarcode("ITEM000001").getDiscount()).isEqualTo(75);
+        assertThat(itemService.getItemByBarcode("ITEM000001").getPromotionList().size()).isEqualTo(3);
 
     }
 }
