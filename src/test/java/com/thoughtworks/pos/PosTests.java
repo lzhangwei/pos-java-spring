7bf6121018059e9f05c4605115577ccaa6b43120
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 16:54:37 +0800

    add before function

--- src/test/java/com/thoughtworks/pos/PosTests.java
+++ src/test/java/com/thoughtworks/pos/PosTests.java
@@ -1,8 +1,20 @@
 package com.thoughtworks.pos;
 
+import com.thoughtworks.pos.service.ItemService;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class PosTests {
+
+    private Pos pos;
+
+    @Before
+    public void before() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        pos = (Pos)context.getBean("pos");
+    }
 
     @Test
     public void test() {
