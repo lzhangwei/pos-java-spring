5db2c5f37e43ad731fb0d95ec0e9089e4877edd1
lzhangwei <zhangwei_999@foxmail.com>
2014-11-07 16:27:12 +0800

    add promotion test file

--- src/test/java/com/thoughtworks/pos/PromotionDiscountTests.java
+++ src/test/java/com/thoughtworks/pos/PromotionDiscountTests.java
@@ -1,14 +1,10 @@
 package com.thoughtworks.pos;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring-config.xml")
 public class PromotionDiscountTests {
-//    @test
+
+    @Test
     public void promotion_discount_caculate_test() {
 
     }
---
+++ src/test/java/com/thoughtworks/pos/model/PromotionTests.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.pos.model;
+
+/**
+ * Created by zhangwei on 14-11-7.
+ */
+public class PromotionTests {
+}
